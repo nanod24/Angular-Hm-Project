5b2a28a8212e7c5fda8f12283ed33312f9311413
diana890 <diananano@yahoo.com>
2024-09-18 07:13:24 -0400

    creating user cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {User} from './Shared/Model/user';
-import {JsonPipe, NgForOf} from "@angular/common"
+import {JsonPipe, NgForOf} from "@angular/common";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,5 +20,9 @@ export class AppComponent {
     {id: 1, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
     {id: 1, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: true},
   ]
-
+//Function that gets called from our onclick. Takes in an
+  //argument ofa variable called user, which is type User and returns void
+  toggleAdminStatus(user: User): void {
+    user.isAdmin = !user.isAdmin;
+  }
 }
