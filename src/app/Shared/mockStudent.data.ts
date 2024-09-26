e0971107e553f5a67879b36cd67b8a3cd7941a2e
diana890 <diananano@yahoo.com>
2024-09-26 11:34:35 -0400

    add new mockdata file

--- src/app/Shared/mockStudent.data.ts
+++ src/app/Shared/mockStudent.data.ts
@@ -8,7 +8,8 @@ import {User} from './Model/user';
 export const userList: User[] = [
   {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
   {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
-  {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-  {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+
+  {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true},
+  {id: 3, firstName: "Diana ", lastName: "Doe", department: "Net", isAdmin: false}
   //Add more as needed
 ];
