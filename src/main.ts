126316ef4d3e7409ffd54f8408bd5f9a8dd1fe47
diana890 <diananano@yahoo.com>
2024-10-03 08:04:57 -0400

    set up routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import {StudentListComponent} from "./app/student-list/student-list.component";
+import {StudentDetailComponent} from "./app/student-detail/student-detail.component";
+
+const routes: Routes = [
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/:id', component: StudentDetailComponent },
+
+]
+
+
+
+
+
+bootstrapApplication(AppComponent, )
+{
+  providers: [provideRouter(routes)]
+};
